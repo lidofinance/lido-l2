ac3d5a28292a1816b90936a822bf424fcf892228
Bogdan Kovtun <psirex48@gmail.com>
2022-06-26 17:21:42 +0300

    Lock the solidity version in the contracts

--- contracts/arbitrum/L2CrossDomainEnabled.sol
+++ contracts/arbitrum/L2CrossDomainEnabled.sol
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Lido <info@lido.fi>
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.10;
 
 import {IArbSys} from "./interfaces/IArbSys.sol";
 
