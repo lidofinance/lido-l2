b5a97fc50773fb7aadbf12a0954e0baf96770b0c
ljankovic-txfusion <lazar@txfusion.io>
2023-06-08 14:41:42 +0200

    remove formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,37 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
 	env: {
-		browser: false,
-		es2021: true,
-		mocha: true,
-		node: true,
+	  browser: false,
+	  es2021: true,
+	  mocha: true,
+	  node: true,
 	},
-	plugins: ['@typescript-eslint'],
+	plugins: ["@typescript-eslint"],
 	extends: [
-		'standard',
-		'plugin:prettier/recommended',
-		'plugin:node/recommended',
+	  "standard",
+	  "plugin:prettier/recommended",
+	  "plugin:node/recommended",
 	],
-	parser: '@typescript-eslint/parser',
+	parser: "@typescript-eslint/parser",
 	parserOptions: {
-		ecmaVersion: 12,
+	  ecmaVersion: 12,
 	},
 	settings: {
-		node: {
-			tryExtensions: ['.js', '.ts'],
-		},
+	  node: {
+		tryExtensions: [".js", ".ts"],
+	  },
 	},
 	globals: {
-		Awaited: 'readonly',
+	  Awaited: "readonly",
 	},
 	rules: {
-		'no-useless-constructor': 'off',
-		camelcase: ['error', { allow: ['.+__factory$'], properties: 'never' }],
-		'node/no-unpublished-import': 'off',
-		'node/no-unsupported-features/es-syntax': [
-			'error',
-			{ ignores: ['modules'], version: '>=12.0.0' },
-		],
+	  "no-useless-constructor": "off",
+	  camelcase: ["error", { allow: [".+__factory$"], properties: "never" }],
+	  "node/no-unpublished-import": "off",
+	  "node/no-unsupported-features/es-syntax": [
+		"error",
+		{ ignores: ["modules"], version: ">=12.0.0" },
+	  ],
 	},
-};
+  };
+  
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,156 +1,147 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
-import 'hardhat-gas-reporter';
-import 'solidity-coverage';
-import './tasks/fork-node';
-import '@matterlabs/hardhat-zksync-solc';
-
-import env from './utils/env';
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "./tasks/fork-node";
+import env from "./utils/env";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-	zksolc: {
-		version: '1.3.6',
-		compilerSource: 'binary',
-		settings: {
-			isSystem: true,
-		},
-	},
-	solidity: {
-		compilers: [
-			{
-				version: '0.6.11',
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 100,
-					},
-				},
-			},
-			{
-				version: '0.8.10',
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 100_000,
-					},
-				},
-			},
-			{
-				version: '0.8.15',
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 1000,
-					},
-				},
-			},
-		],
-	},
-	networks: {
-		// Ethereum Public Chains
-		eth_mainnet: {
-			url: env.string('RPC_ETH_MAINNET', ''),
-		},
-		eth_goerli: {
-			url: env.string('RPC_ETH_GOERLI', ''),
-		},
-		eth_kovan: {
-			url: env.string('RPC_ETH_KOVAN', ''),
-		},
-		eth_rinkeby: {
-			url: env.string('RPC_ETH_RINKEBY', ''),
-		},
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.11",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+        },
+      },
+      {
+        version: "0.8.10",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100_000,
+          },
+        },
+      },
+      {
+        version: "0.8.15",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    // Ethereum Public Chains
+    eth_mainnet: {
+      url: env.string("RPC_ETH_MAINNET", ""),
+    },
+    eth_goerli: {
+      url: env.string("RPC_ETH_GOERLI", ""),
+    },
+    eth_kovan: {
+      url: env.string("RPC_ETH_KOVAN", ""),
+    },
+    eth_rinkeby: {
+      url: env.string("RPC_ETH_RINKEBY", ""),
+    },
 
-		// Ethereum Fork Chains
-		eth_mainnet_fork: {
-			url: 'http://localhost:8545',
-		},
-		eth_goerli_fork: {
-			url: 'http://localhost:8545',
-		},
-		eth_kovan_fork: {
-			url: 'http://localhost:8545',
-		},
-		eth_rinkeby_fork: {
-			url: 'http://localhost:8545',
-		},
+    // Ethereum Fork Chains
+    eth_mainnet_fork: {
+      url: "http://localhost:8545",
+    },
+    eth_goerli_fork: {
+      url: "http://localhost:8545",
+    },
+    eth_kovan_fork: {
+      url: "http://localhost:8545",
+    },
+    eth_rinkeby_fork: {
+      url: "http://localhost:8545",
+    },
 
-		// Arbitrum Public Chains
-		arb_mainnet: {
-			url: env.string('RPC_ARB_MAINNET', ''),
-		},
-		arb_goerli: {
-			url: env.string('RPC_ARB_GOERLI', ''),
-		},
-		arb_rinkeby: {
-			url: env.string('RPC_ARB_RINKEBY', ''),
-		},
+    // Arbitrum Public Chains
+    arb_mainnet: {
+      url: env.string("RPC_ARB_MAINNET", ""),
+    },
+    arb_goerli: {
+      url: env.string("RPC_ARB_GOERLI", ""),
+    },
+    arb_rinkeby: {
+      url: env.string("RPC_ARB_RINKEBY", ""),
+    },
 
-		// Arbitrum Fork Chains
-		arb_mainnet_fork: {
-			url: 'http://localhost:8546',
-		},
-		arb_goerli_fork: {
-			url: 'http://localhost:8546',
-		},
-		arb_rinkeby_fork: {
-			url: 'http://localhost:8546',
-		},
+    // Arbitrum Fork Chains
+    arb_mainnet_fork: {
+      url: "http://localhost:8546",
+    },
+    arb_goerli_fork: {
+      url: "http://localhost:8546",
+    },
+    arb_rinkeby_fork: {
+      url: "http://localhost:8546",
+    },
 
-		// Optimism Public Chains
-		opt_mainnet: {
-			url: env.string('RPC_OPT_MAINNET', ''),
-		},
-		opt_goerli: {
-			url: env.string('RPC_OPT_GOERLI', ''),
-		},
-		opt_kovan: {
-			url: env.string('RPC_OPT_KOVAN', ''),
-		},
+    // Optimism Public Chains
+    opt_mainnet: {
+      url: env.string("RPC_OPT_MAINNET", ""),
+    },
+    opt_goerli: {
+      url: env.string("RPC_OPT_GOERLI", ""),
+    },
+    opt_kovan: {
+      url: env.string("RPC_OPT_KOVAN", ""),
+    },
 
-		// Optimism Fork Chains
-		opt_mainnet_fork: {
-			url: 'http://localhost:9545',
-		},
-		opt_goerli_fork: {
-			url: 'http://localhost:9545',
-		},
-		opt_kovan_fork: {
-			url: 'http://localhost:9545',
-		},
-	},
-	gasReporter: {
-		enabled: env.string('REPORT_GAS', 'false') !== 'false',
-		currency: 'USD',
-	},
-	etherscan: {
-		apiKey: {
-			kovan: env.string('ETHERSCAN_API_KEY_ETH', ''),
-			rinkeby: env.string('ETHERSCAN_API_KEY_ETH', ''),
-			mainnet: env.string('ETHERSCAN_API_KEY_ETH', ''),
-			goerli: env.string('ETHERSCAN_API_KEY_ETH', ''),
-			arbitrumTestnet: env.string('ETHERSCAN_API_KEY_ARB', ''),
-			arbitrumOne: env.string('ETHERSCAN_API_KEY_ARB', ''),
-			optimisticKovan: env.string('ETHERSCAN_API_KEY_OPT', ''),
-			optimisticEthereum: env.string('ETHERSCAN_API_KEY_OPT', ''),
-		},
-	},
-	typechain: {
-		externalArtifacts: [
-			'./interfaces/**/*.json',
-			'./utils/optimism/artifacts/*.json',
-			'./utils/arbitrum/artifacts/*.json',
-		],
-	},
-	mocha: {
-		timeout: 20 * 60 * 60 * 1000, // 20 minutes for e2e tests
-	},
+    // Optimism Fork Chains
+    opt_mainnet_fork: {
+      url: "http://localhost:9545",
+    },
+    opt_goerli_fork: {
+      url: "http://localhost:9545",
+    },
+    opt_kovan_fork: {
+      url: "http://localhost:9545",
+    },
+  },
+  gasReporter: {
+    enabled: env.string("REPORT_GAS", "false") !== "false",
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: {
+      kovan: env.string("ETHERSCAN_API_KEY_ETH", ""),
+      rinkeby: env.string("ETHERSCAN_API_KEY_ETH", ""),
+      mainnet: env.string("ETHERSCAN_API_KEY_ETH", ""),
+      goerli: env.string("ETHERSCAN_API_KEY_ETH", ""),
+      arbitrumTestnet: env.string("ETHERSCAN_API_KEY_ARB", ""),
+      arbitrumOne: env.string("ETHERSCAN_API_KEY_ARB", ""),
+      optimisticKovan: env.string("ETHERSCAN_API_KEY_OPT", ""),
+      optimisticEthereum: env.string("ETHERSCAN_API_KEY_OPT", ""),
+    },
+  },
+  typechain: {
+    externalArtifacts: [
+      "./interfaces/**/*.json",
+      "./utils/optimism/artifacts/*.json",
+      "./utils/arbitrum/artifacts/*.json",
+    ],
+  },
+  mocha: {
+    timeout: 20 * 60 * 60 * 1000, // 20 minutes for e2e tests
+  },
 };
 
 export default config;
