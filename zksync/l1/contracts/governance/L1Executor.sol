120fb8da3689369d89986064578fccfc7ea87e2e
ljankovic-txfusion <lazar@txfusion.io>
2023-07-06 11:56:19 +0200

    resolved conflicts

--- zksync/l1/contracts/governance/L1Executor.sol
+++ zksync/l1/contracts/governance/L1Executor.sol
@@ -2,8 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {IZkSync} from "@matterlabs/zksync-contracts/l1/contracts/zksync/interfaces/IZkSync.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract L1Executor {
+contract L1Executor is OwnableUpgradeable {
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() external initializer {
+        __Ownable_init();
+    }
+
     function callZkSync(
         address zkSyncAddress,
         address contractAddr,
@@ -11,7 +20,6 @@ contract L1Executor {
         uint256 gasLimit,
         uint256 gasPerPubdataByteLimit
     ) external payable {
-
         IZkSync zksync = IZkSync(zkSyncAddress);
         zksync.requestL2Transaction{value: msg.value}(
             contractAddr,
--- zksync/l1/scripts/deploy-l1-executor.ts
+++ zksync/l1/scripts/deploy-l1-executor.ts
@@ -1,30 +1,49 @@
 /* eslint-disable prettier/prettier */
-import { web3Provider } from './utils/utils';
-import { OssifiableProxy__factory } from '../typechain/index';
-import { L1Executor__factory } from '../typechain/index';
-import { Wallet } from 'ethers';
+import { web3Provider } from "./utils/utils";
+import { OssifiableProxy__factory } from "../typechain/index";
+import { L1Executor__factory } from "../typechain";
+import { Wallet } from "ethers";
 
 const provider = web3Provider();
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const AGENT_ADDRESS = process.env.CONTRACTS_L1_GOVERNANCE_AGENT_ADDR || "";
 
 async function main() {
-	// without ethers.Wallet -> HardhatError: HH5: HardhatContext is not created.
-	const wallet = new Wallet(PRIVATE_KEY, provider);
+  // without ethers.Wallet -> HardhatError: HH5: HardhatContext is not created.
+  const wallet = new Wallet(PRIVATE_KEY, provider);
 
-	const L1ExecutorContractImpl = await new L1Executor__factory(wallet).deploy();
+  const L1ExecutorContractImpl = await new L1Executor__factory(wallet).deploy();
 
-	console.log(`L1Executor implementation:${L1ExecutorContractImpl.address}`);
+  console.log(`L1Executor implementation:${L1ExecutorContractImpl.address}`);
 
-	const L1ExecutorContractProxy = await new OssifiableProxy__factory(
-		wallet
-	).deploy(L1ExecutorContractImpl.address, wallet.address, '0x', {
-		gasLimit: 10_000_000,
-	});
+  const L1ExecutorContractProxy = await new OssifiableProxy__factory(
+    wallet
+  ).deploy(L1ExecutorContractImpl.address, AGENT_ADDRESS, "0x", {
+    gasLimit: 10_000_000,
+  });
 
-	console.log(`L1_EXECUTOR_ADDR=${L1ExecutorContractProxy.address}`);
+  console.log(`L1Executor proxy:${L1ExecutorContractProxy.address}`);
+
+  const L1Executor = new L1Executor__factory(wallet).attach(
+    L1ExecutorContractProxy.address
+  );
+
+  console.log(`L1Executor: ${L1Executor.address}`);
+
+  const initResponseTx = await L1Executor.initialize({ gasLimit: 10_000_000 });
+  await initResponseTx.wait();
+  const transferOwnerResponseTx = await L1Executor.transferOwnership(
+    AGENT_ADDRESS
+  );
+  await transferOwnerResponseTx.wait();
+
+  console.log("Owner of the L1 Executor:", await L1Executor.owner());
+  console.log(
+    `CONTRACTS_L1_GOVERNANCE_EXECUTOR_ADDR=${L1ExecutorContractProxy.address}`
+  );
 }
 
 main().catch((error) => {
-	throw error;
+  throw error;
 });
