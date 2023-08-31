8d36633d404ddbba8ad264faa62922bd4217750b
ljankovic-txfusion <lazar@txfusion.io>
2023-08-31 09:10:47 +0200

    add comment for aliasing governance executor

--- zksync/l2/scripts/deploy-governance-bridge.ts
+++ zksync/l2/scripts/deploy-governance-bridge.ts
@@ -18,6 +18,11 @@ async function main() {
 
   const artifact = await deployer.loadArtifact(BRIDGE_EXECUTOR_CONTRACT_NAME);
 
+  /**
+   * If the governance executor is a contract on L1, alias the contract address from L1 to L2
+   * e.g. utils.applyL1ToL2Alias(contractAddressOnL1)
+   * If the governance executor is an EOA, no aliasing is needed
+   */
   const l2AddressOfL1Executor = utils.applyL1ToL2Alias(
     ADDRESSES.L1_EXECUTOR_ADDR
   );
