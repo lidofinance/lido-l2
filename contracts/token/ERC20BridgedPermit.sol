db3eabbc40995386286ddd6b39dce4858e58b69e
afkbyte <afkbyte@protonmail.com>
2023-09-01 05:08:36 -0400

    new implementation without formatting issues

---
+++ contracts/token/ERC20BridgedPermit.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.10;
+
+import {ERC20Bridged} from "./ERC20Bridged.sol";
+import {IERC2612} from "@openzeppelin/contracts/interfaces/draft-IERC2612.sol";
+
+/// @author 0xMantle
+/// @notice Extends the ERC20 functionality that allows the bridge to mint/burn tokens
+contract ERC20BridgedPermit is ERC20Bridged, IERC2612 {
+
+    mapping(address => uint256) public override nonces;
+
+    bytes32 public immutable PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, address bridge_)
+        ERC20Bridged(name_, symbol_, decimals_, bridge_){}
+
+    function _domainSeparatorV4() internal view returns (bytes32) {
+        return _buildDomainSeparator();
+    }
+
+    function _buildDomainSeparator() private view returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"), 
+                keccak256(bytes(name())), 
+                keccak256(bytes(version())), 
+                block.chainid, 
+                address(this)
+            )
+        );
+    }
+
+    function DOMAIN_SEPARATOR() external view virtual returns (bytes32) {
+        return _domainSeparatorV4();
+    }
+
+
+    function version() public pure virtual returns (string memory) {
+        return "1";
+    }
+
+    function permit(address owner, address spender, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        public
+        virtual
+        override
+    {
+        require(deadline >= block.timestamp, "ERC20Permit: expired deadline");
+
+        bytes32 hashStruct = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, amount, nonces[owner]++, deadline));
+
+        bytes32 hash = keccak256(abi.encodePacked("\x19\x01", _domainSeparatorV4(), hashStruct));
+
+        address signer = ecrecover(hash, v, r, s);
+        require(signer != address(0) && signer == owner, "ERC20Permit: invalid signature");
+
+        _approve(owner, spender, amount);
+    }
+}
---
+++ test/token/ERC20BridgedPermit.unit.test.ts
@@ -0,0 +1,102 @@
+import { assert } from "chai";
+import hre from "hardhat";
+import {
+  ERC20BridgedPermit__factory,
+  OssifiableProxy__factory,
+} from "../../typechain";
+import { unit } from "../../utils/testing";
+import { wei } from "../../utils/wei";
+import { getPermitSignature } from "../../utils/testing/permit";
+import { constants } from 'ethers'
+
+unit("ERC20BridgedPermit", ctxFactory)
+  .test("bridge()", async (ctx) => {
+    assert.equal(await ctx.erc20BridgedPermit.bridge(), ctx.accounts.owner.address);
+  })
+
+  .test("totalSupply()", async (ctx) => {
+    assert.equalBN(await ctx.erc20BridgedPermit.totalSupply(), ctx.constants.premint);
+  })
+
+  .test("initialize() :: name already set", async (ctx) => {
+    const { deployer, owner } = ctx.accounts;
+
+    // deploy new implementation
+    const erc20BridgedPermitImpl = await new ERC20BridgedPermit__factory(deployer).deploy(
+      "Name",
+      "",
+      9,
+      owner.address
+    );
+    await assert.revertsWith(
+      erc20BridgedPermitImpl.initialize("New Name", ""),
+      "ErrorNameAlreadySet()"
+    );
+  })
+
+  .test("permit() :: valid signature", async (ctx) => {
+    const { erc20BridgedPermit } = ctx;
+    const { holder, spender } = ctx.accounts;
+    const value = 123
+
+    const { v, r, s } = await getPermitSignature(holder, erc20BridgedPermit, spender.address, value)
+
+    assert.equalBN(await erc20BridgedPermit.allowance(holder.address, spender.address), 0)
+    await erc20BridgedPermit.permit(
+      holder.address,
+      spender.address,
+      value,
+      constants.MaxUint256,
+      v,
+      r,
+      s
+    )
+
+    assert.equalBN(await erc20BridgedPermit.allowance(holder.address, spender.address), value)
+  })
+
+  .run();
+
+  async function ctxFactory() {
+    const name = "ERC20 Test Token";
+    const symbol = "ERC20";
+    const decimals = 18;
+    const premint = wei`100 ether`;
+    const [deployer, owner, recipient, spender, holder, stranger] =
+      await hre.ethers.getSigners();
+    const l2TokenImpl = await new ERC20BridgedPermit__factory(deployer).deploy(
+      name,
+      symbol,
+      decimals,
+      owner.address
+    );
+
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [hre.ethers.constants.AddressZero],
+    });
+
+    const zero = await hre.ethers.getSigner(hre.ethers.constants.AddressZero);
+
+    const l2TokensProxy = await new OssifiableProxy__factory(deployer).deploy(
+      l2TokenImpl.address,
+      deployer.address,
+      ERC20BridgedPermit__factory.createInterface().encodeFunctionData("initialize", [
+        name,
+        symbol,
+      ])
+    );
+
+    const erc20BridgedPermitProxied = ERC20BridgedPermit__factory.connect(
+      l2TokensProxy.address,
+      holder
+    );
+    ERC20BridgedPermit__factory
+    await erc20BridgedPermitProxied.connect(owner).bridgeMint(holder.address, premint);
+
+    return {
+      accounts: { deployer, owner, recipient, spender, holder, zero, stranger },
+      constants: { name, symbol, decimals, premint },
+      erc20BridgedPermit: erc20BridgedPermitProxied,
+    };
+  }
