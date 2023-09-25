67e7cc0a922d72a101c9ea4f2d50ad594ad9e709
Morteza <morteza@txfusion.io>
2023-09-25 11:55:38 +0330

    add conditional defaultNetwork field on l1 hardhat.config.ts (verification needs this field)

--- zksync/l1/hardhat.config.ts
+++ zksync/l1/hardhat.config.ts
@@ -6,6 +6,10 @@ import "@nomicfoundation/hardhat-verify";
 
 dotenv.config({ path: `../.env` });
 
+const IS_LOCAL = (process.env.CHAIN_ETH_NETWORK as string) === "localhost";
+const L1_DEFAULT_NETWORK = (process.env.L1_DEFAULT_NETWORK ||
+  "goerli") as string;
+
 const config: HardhatUserConfig & { etherscan: { apiKey: string } } = {
   solidity: {
     version: "0.8.15",
@@ -21,6 +25,7 @@ const config: HardhatUserConfig & { etherscan: { apiKey: string } } = {
       url: process.env.ETH_CLIENT_WEB3_URL as string,
     },
   },
+  ...(!IS_LOCAL && { defaultNetwork: L1_DEFAULT_NETWORK }),
   etherscan: {
     apiKey: process.env.ETHER_SCAN_API_KEY as string,
   },
