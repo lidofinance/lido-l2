a574aeee33506b903c8128944e19628b9f39e356
ljankovic-txfusion <lazar@txfusion.io>
2023-11-08 11:19:35 +0100

    Revert "changed L2 governance licenses"

--- zksync/l2/contracts/governance/interfaces/IExecutorBase.sol
+++ zksync/l2/contracts/governance/interfaces/IExecutorBase.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: AGPL-3.0
+// SPDX-FileCopyrightText: 2023 Lido <info@lido.fi>
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.10;
 
@@ -8,228 +9,236 @@ pragma solidity ^0.8.10;
  * @notice Defines the basic interface for the ExecutorBase abstract contract
  */
 interface IExecutorBase {
-  error InvalidInitParams();
-  error NotGuardian();
-  error OnlyCallableByThis();
-  error MinimumDelayTooLong();
-  error MaximumDelayTooShort();
-  error MaximumDelayTooLong();
-  error GracePeriodTooShort();
-  error DelayShorterThanMin();
-  error DelayLongerThanMax();
-  error OnlyQueuedActions();
-  error TimelockNotFinished();
-  error InvalidActionsSetId();
-  error EmptyTargets();
-  error TooManyTargets();
-  error InconsistentParamsLength();
-  error DuplicateAction();
-  error InsufficientBalance();
-  error FailedActionExecution();
+    error InvalidInitParams();
+    error NotGuardian();
+    error OnlyCallableByThis();
+    error MinimumDelayTooLong();
+    error MaximumDelayTooShort();
+    error MaximumDelayTooLong();
+    error GracePeriodTooShort();
+    error DelayShorterThanMin();
+    error DelayLongerThanMax();
+    error OnlyQueuedActions();
+    error TimelockNotFinished();
+    error InvalidActionsSetId();
+    error EmptyTargets();
+    error TooManyTargets();
+    error InconsistentParamsLength();
+    error DuplicateAction();
+    error InsufficientBalance();
+    error FailedActionExecution();
 
-  /**
-   * @notice This enum contains all possible actions set states
-   */
-  enum ActionsSetState {
-    Queued,
-    Executed,
-    Canceled,
-    Expired
-  }
+    /**
+     * @notice This enum contains all possible actions set states
+     */
+    enum ActionsSetState {
+        Queued,
+        Executed,
+        Canceled,
+        Expired
+    }
 
-  /**
-   * @notice This struct contains the data needed to execute a specified set of actions
-   * @param targets Array of targets to call
-   * @param values Array of values to pass in each call
-   * @param signatures Array of function signatures to encode in each call (can be empty)
-   * @param calldatas Array of calldatas to pass in each call, appended to the signature at the same array index if not empty
-   * @param executionTime Timestamp starting from which the actions set can be executed
-   * @param expireTime Timestamp after which the action is considered expired
-   * @param executed True if the actions set has been executed, false otherwise
-   * @param canceled True if the actions set has been canceled, false otherwise
-   */
-  struct ActionsSet {
-    address[] targets;
-    uint256[] values;
-    string[] signatures;
-    bytes[] calldatas;
-    uint256 executionTime;
-    uint256 expireTime;
-    bool executed;
-    bool canceled;
-  }
+    /**
+     * @notice This struct contains the data needed to execute a specified set of actions
+     * @param targets Array of targets to call
+     * @param values Array of values to pass in each call
+     * @param signatures Array of function signatures to encode in each call (can be empty)
+     * @param calldatas Array of calldatas to pass in each call, appended to the signature at the same array index if not empty
+     * @param executionTime Timestamp starting from which the actions set can be executed
+     * @param expireTime Timestamp after which the action is considered expired
+     * @param executed True if the actions set has been executed, false otherwise
+     * @param canceled True if the actions set has been canceled, false otherwise
+     */
+    struct ActionsSet {
+        address[] targets;
+        uint256[] values;
+        string[] signatures;
+        bytes[] calldatas;
+        uint256 executionTime;
+        uint256 expireTime;
+        bool executed;
+        bool canceled;
+    }
 
-  /**
-   * @dev Emitted when an ActionsSet is queued
-   * @param id Id of the ActionsSet
-   * @param targets Array of targets to be called by the actions set
-   * @param values Array of values to pass in each call by the actions set
-   * @param signatures Array of function signatures to encode in each call by the actions set
-   * @param calldatas Array of calldata to pass in each call by the actions set
-   * @param executionTime The timestamp at which this actions set can be executed
-   **/
-  event ActionsSetQueued(
-    uint256 indexed id,
-    address[] targets,
-    uint256[] values,
-    string[] signatures,
-    bytes[] calldatas,
-    uint256 executionTime
-  );
+    /**
+     * @dev Emitted when an ActionsSet is queued
+     * @param id Id of the ActionsSet
+     * @param targets Array of targets to be called by the actions set
+     * @param values Array of values to pass in each call by the actions set
+     * @param signatures Array of function signatures to encode in each call by the actions set
+     * @param calldatas Array of calldata to pass in each call by the actions set
+     * @param executionTime The timestamp at which this actions set can be executed
+     **/
+    event ActionsSetQueued(
+        uint256 indexed id,
+        address[] targets,
+        uint256[] values,
+        string[] signatures,
+        bytes[] calldatas,
+        uint256 executionTime
+    );
 
-  /**
-   * @dev Emitted when an ActionsSet is successfully executed
-   * @param id Id of the ActionsSet
-   * @param initiatorExecution The address that triggered the ActionsSet execution
-   * @param returnedData The returned data from the ActionsSet execution
-   **/
-  event ActionsSetExecuted(uint256 indexed id, address indexed initiatorExecution, bytes[] returnedData);
+    /**
+     * @dev Emitted when an ActionsSet is successfully executed
+     * @param id Id of the ActionsSet
+     * @param initiatorExecution The address that triggered the ActionsSet execution
+     * @param returnedData The returned data from the ActionsSet execution
+     **/
+    event ActionsSetExecuted(
+        uint256 indexed id,
+        address indexed initiatorExecution,
+        bytes[] returnedData
+    );
 
-  /**
-   * @dev Emitted when an ActionsSet is cancelled by the guardian
-   * @param id Id of the ActionsSet
-   **/
-  event ActionsSetCanceled(uint256 indexed id);
+    /**
+     * @dev Emitted when an ActionsSet is cancelled by the guardian
+     * @param id Id of the ActionsSet
+     **/
+    event ActionsSetCanceled(uint256 indexed id);
 
-  /**
-   * @dev Emitted when a new guardian is set
-   * @param oldGuardian The address of the old guardian
-   * @param newGuardian The address of the new guardian
-   **/
-  event GuardianUpdate(address oldGuardian, address newGuardian);
+    /**
+     * @dev Emitted when a new guardian is set
+     * @param oldGuardian The address of the old guardian
+     * @param newGuardian The address of the new guardian
+     **/
+    event GuardianUpdate(address oldGuardian, address newGuardian);
 
-  /**
-   * @dev Emitted when the delay (between queueing and execution) is updated
-   * @param oldDelay The value of the old delay
-   * @param newDelay The value of the new delay
-   **/
-  event DelayUpdate(uint256 oldDelay, uint256 newDelay);
+    /**
+     * @dev Emitted when the delay (between queueing and execution) is updated
+     * @param oldDelay The value of the old delay
+     * @param newDelay The value of the new delay
+     **/
+    event DelayUpdate(uint256 oldDelay, uint256 newDelay);
 
-  /**
-   * @dev Emitted when the grace period (between executionTime and expiration) is updated
-   * @param oldGracePeriod The value of the old grace period
-   * @param newGracePeriod The value of the new grace period
-   **/
-  event GracePeriodUpdate(uint256 oldGracePeriod, uint256 newGracePeriod);
+    /**
+     * @dev Emitted when the grace period (between executionTime and expiration) is updated
+     * @param oldGracePeriod The value of the old grace period
+     * @param newGracePeriod The value of the new grace period
+     **/
+    event GracePeriodUpdate(uint256 oldGracePeriod, uint256 newGracePeriod);
 
-  /**
-   * @dev Emitted when the minimum delay (lower bound of delay) is updated
-   * @param oldMinimumDelay The value of the old minimum delay
-   * @param newMinimumDelay The value of the new minimum delay
-   **/
-  event MinimumDelayUpdate(uint256 oldMinimumDelay, uint256 newMinimumDelay);
+    /**
+     * @dev Emitted when the minimum delay (lower bound of delay) is updated
+     * @param oldMinimumDelay The value of the old minimum delay
+     * @param newMinimumDelay The value of the new minimum delay
+     **/
+    event MinimumDelayUpdate(uint256 oldMinimumDelay, uint256 newMinimumDelay);
 
-  /**
-   * @dev Emitted when the maximum delay (upper bound of delay)is updated
-   * @param oldMaximumDelay The value of the old maximum delay
-   * @param newMaximumDelay The value of the new maximum delay
-   **/
-  event MaximumDelayUpdate(uint256 oldMaximumDelay, uint256 newMaximumDelay);
+    /**
+     * @dev Emitted when the maximum delay (upper bound of delay)is updated
+     * @param oldMaximumDelay The value of the old maximum delay
+     * @param newMaximumDelay The value of the new maximum delay
+     **/
+    event MaximumDelayUpdate(uint256 oldMaximumDelay, uint256 newMaximumDelay);
 
-  /**
-   * @notice Execute the ActionsSet
-   * @param actionsSetId The id of the ActionsSet to execute
-   **/
-  function execute(uint256 actionsSetId) external payable;
+    /**
+     * @notice Execute the ActionsSet
+     * @param actionsSetId The id of the ActionsSet to execute
+     **/
+    function execute(uint256 actionsSetId) external payable;
 
-  /**
-   * @notice Cancel the ActionsSet
-   * @param actionsSetId The id of the ActionsSet to cancel
-   **/
-  function cancel(uint256 actionsSetId) external;
+    /**
+     * @notice Cancel the ActionsSet
+     * @param actionsSetId The id of the ActionsSet to cancel
+     **/
+    function cancel(uint256 actionsSetId) external;
 
-  /**
-   * @notice Update guardian
-   * @param guardian The address of the new guardian
-   **/
-  function updateGuardian(address guardian) external;
+    /**
+     * @notice Update guardian
+     * @param guardian The address of the new guardian
+     **/
+    function updateGuardian(address guardian) external;
 
-  /**
-   * @notice Update the delay, time between queueing and execution of ActionsSet
-   * @dev It does not affect to actions set that are already queued
-   * @param delay The value of the delay (in seconds)
-   **/
-  function updateDelay(uint256 delay) external;
+    /**
+     * @notice Update the delay, time between queueing and execution of ActionsSet
+     * @dev It does not affect to actions set that are already queued
+     * @param delay The value of the delay (in seconds)
+     **/
+    function updateDelay(uint256 delay) external;
 
-  /**
-   * @notice Update the grace period, the period after the execution time during which an actions set can be executed
-   * @param gracePeriod The value of the grace period (in seconds)
-   **/
-  function updateGracePeriod(uint256 gracePeriod) external;
+    /**
+     * @notice Update the grace period, the period after the execution time during which an actions set can be executed
+     * @param gracePeriod The value of the grace period (in seconds)
+     **/
+    function updateGracePeriod(uint256 gracePeriod) external;
 
-  /**
-   * @notice Update the minimum allowed delay
-   * @param minimumDelay The value of the minimum delay (in seconds)
-   **/
-  function updateMinimumDelay(uint256 minimumDelay) external;
+    /**
+     * @notice Update the minimum allowed delay
+     * @param minimumDelay The value of the minimum delay (in seconds)
+     **/
+    function updateMinimumDelay(uint256 minimumDelay) external;
 
-  /**
-   * @notice Update the maximum allowed delay
-   * @param maximumDelay The maximum delay (in seconds)
-   **/
-  function updateMaximumDelay(uint256 maximumDelay) external;
+    /**
+     * @notice Update the maximum allowed delay
+     * @param maximumDelay The maximum delay (in seconds)
+     **/
+    function updateMaximumDelay(uint256 maximumDelay) external;
 
-  /**
-   * @notice Allows to receive funds into the executor
-   * @dev Useful for actionsSet that needs funds to gets executed
-   */
-  function receiveFunds() external payable;
+    /**
+     * @notice Allows to receive funds into the executor
+     * @dev Useful for actionsSet that needs funds to gets executed
+     */
+    function receiveFunds() external payable;
 
-  /**
-   * @notice Returns the delay (between queuing and execution)
-   * @return The value of the delay (in seconds)
-   **/
-  function getDelay() external view returns (uint256);
+    /**
+     * @notice Returns the delay (between queuing and execution)
+     * @return The value of the delay (in seconds)
+     **/
+    function getDelay() external view returns (uint256);
 
-  /**
-   * @notice Returns the grace period
-   * @return The value of the grace period (in seconds)
-   **/
-  function getGracePeriod() external view returns (uint256);
+    /**
+     * @notice Returns the grace period
+     * @return The value of the grace period (in seconds)
+     **/
+    function getGracePeriod() external view returns (uint256);
 
-  /**
-   * @notice Returns the minimum delay
-   * @return The value of the minimum delay (in seconds)
-   **/
-  function getMinimumDelay() external view returns (uint256);
+    /**
+     * @notice Returns the minimum delay
+     * @return The value of the minimum delay (in seconds)
+     **/
+    function getMinimumDelay() external view returns (uint256);
 
-  /**
-   * @notice Returns the maximum delay
-   * @return The value of the maximum delay (in seconds)
-   **/
-  function getMaximumDelay() external view returns (uint256);
+    /**
+     * @notice Returns the maximum delay
+     * @return The value of the maximum delay (in seconds)
+     **/
+    function getMaximumDelay() external view returns (uint256);
 
-  /**
-   * @notice Returns the address of the guardian
-   * @return The address of the guardian
-   **/
-  function getGuardian() external view returns (address);
+    /**
+     * @notice Returns the address of the guardian
+     * @return The address of the guardian
+     **/
+    function getGuardian() external view returns (address);
 
-  /**
-   * @notice Returns the total number of actions sets of the executor
-   * @return The number of actions sets
-   **/
-  function getActionsSetCount() external view returns (uint256);
+    /**
+     * @notice Returns the total number of actions sets of the executor
+     * @return The number of actions sets
+     **/
+    function getActionsSetCount() external view returns (uint256);
 
-  /**
-   * @notice Returns the data of an actions set
-   * @param actionsSetId The id of the ActionsSet
-   * @return The data of the ActionsSet
-   **/
-  function getActionsSetById(uint256 actionsSetId) external view returns (ActionsSet memory);
+    /**
+     * @notice Returns the data of an actions set
+     * @param actionsSetId The id of the ActionsSet
+     * @return The data of the ActionsSet
+     **/
+    function getActionsSetById(
+        uint256 actionsSetId
+    ) external view returns (ActionsSet memory);
 
-  /**
-   * @notice Returns the current state of an actions set
-   * @param actionsSetId The id of the ActionsSet
-   * @return The current state of theI ActionsSet
-   **/
-  function getCurrentState(uint256 actionsSetId) external view returns (ActionsSetState);
+    /**
+     * @notice Returns the current state of an actions set
+     * @param actionsSetId The id of the ActionsSet
+     * @return The current state of theI ActionsSet
+     **/
+    function getCurrentState(
+        uint256 actionsSetId
+    ) external view returns (ActionsSetState);
 
-  /**
-   * @notice Returns whether an actions set (by actionHash) is queued
-   * @dev actionHash = keccak256(abi.encode(target, value, signature, data, executionTime))
-   * @param actionHash hash of the action to be checked
-   * @return True if the underlying action of actionHash is queued, false otherwise
-   **/
-  function isActionQueued(bytes32 actionHash) external view returns (bool);
+    /**
+     * @notice Returns whether an actions set (by actionHash) is queued
+     * @dev actionHash = keccak256(abi.encode(target, value, signature, data, executionTime))
+     * @param actionHash hash of the action to be checked
+     * @return True if the underlying action of actionHash is queued, false otherwise
+     **/
+    function isActionQueued(bytes32 actionHash) external view returns (bool);
 }
--- zksync/l2/contracts/governance/interfaces/IL2BridgeExecutor.sol
+++ zksync/l2/contracts/governance/interfaces/IL2BridgeExecutor.sol
@@ -1,8 +1,9 @@
-// SPDX-License-Identifier: AGPL-3.0
+// SPDX-FileCopyrightText: 2023 Lido <info@lido.fi>
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.10;
 
-import { IExecutorBase } from "./IExecutorBase.sol";
+import {IExecutorBase} from "./IExecutorBase.sol";
 
 /**
  * @title IL2BridgeExecutorBase
@@ -10,39 +11,44 @@ import { IExecutorBase } from "./IExecutorBase.sol";
  * @notice Defines the basic interface for the L2BridgeExecutor abstract contract
  */
 interface IL2BridgeExecutor is IExecutorBase {
-  error UnauthorizedEthereumExecutor();
+    error UnauthorizedEthereumExecutor();
 
-  /**
-   * @dev Emitted when the Ethereum Governance Executor is updated
-   * @param oldEthereumGovernanceExecutor The address of the old EthereumGovernanceExecutor
-   * @param newEthereumGovernanceExecutor The address of the new EthereumGovernanceExecutor
-   **/
-  event EthereumGovernanceExecutorUpdate(address oldEthereumGovernanceExecutor, address newEthereumGovernanceExecutor);
+    /**
+     * @dev Emitted when the Ethereum Governance Executor is updated
+     * @param oldEthereumGovernanceExecutor The address of the old EthereumGovernanceExecutor
+     * @param newEthereumGovernanceExecutor The address of the new EthereumGovernanceExecutor
+     **/
+    event EthereumGovernanceExecutorUpdate(
+        address oldEthereumGovernanceExecutor,
+        address newEthereumGovernanceExecutor
+    );
 
-  /**
-   * @notice Queue an ActionsSet
-   * @dev If a signature is empty, calldata is used for the execution, calldata is appended to signature otherwise
-   * @param targets Array of targets to be called by the actions set
-   * @param values Array of values to pass in each call by the actions set
-   * @param signatures Array of function signatures to encode in each call by the actions (can be empty)
-   * @param calldatas Array of calldata to pass in each call by the actions set
-   **/
-  function queue(
-    address[] memory targets,
-    uint256[] memory values,
-    string[] memory signatures,
-    bytes[] memory calldatas
-  ) external;
+    /**
+     * @notice Queue an ActionsSet
+     * @dev If a signature is empty, calldata is used for the execution, calldata is appended to signature otherwise
+     * @param targets Array of targets to be called by the actions set
+     * @param values Array of values to pass in each call by the actions set
+     * @param signatures Array of function signatures to encode in each call by the actions (can be empty)
+     * @param calldatas Array of calldata to pass in each call by the actions set
+     **/
+    function queue(
+        address[] memory targets,
+        uint256[] memory values,
+        string[] memory signatures,
+        bytes[] memory calldatas
+    ) external;
 
-  /**
-   * @notice Update the address of the Ethereum Governance Executor
-   * @param ethereumGovernanceExecutor The address of the new EthereumGovernanceExecutor which can be EOA or an alias of a contract on L1
-   **/
-  function updateEthereumGovernanceExecutor(address ethereumGovernanceExecutor) external;
+    /**
+     * @notice Update the address of the Ethereum Governance Executor
+     * @param ethereumGovernanceExecutor The address of the new EthereumGovernanceExecutor which can be EOA or an alias of a contract on L1
+     **/
+    function updateEthereumGovernanceExecutor(
+        address ethereumGovernanceExecutor
+    ) external;
 
-  /**
-   * @notice Returns the address of the Ethereum Governance Executor
-   * @return The address of the EthereumGovernanceExecutor which can be EOA or an alias of a contract on L1
-   **/
-  function getEthereumGovernanceExecutor() external view returns (address);
+    /**
+     * @notice Returns the address of the Ethereum Governance Executor
+     * @return The address of the EthereumGovernanceExecutor which can be EOA or an alias of a contract on L1
+     **/
+    function getEthereumGovernanceExecutor() external view returns (address);
 }
