f46fbc02facbc67ef208603185aea7d1edbbb983
Artyom Veremeenko <arwer13@gmail.com>
2024-01-16 16:02:20 +0300

    add linea code from https://github.com/Consensys/linea-contracts-fix
    commit 2056d470143f66196184fd55b27f1240d5f74ef9 from main

---
+++ scripts/deployment/deployVerifier.ts
@@ -0,0 +1,41 @@
+import { run } from "hardhat";
+import { delay } from "../../utils/storeAddress";
+import { deployFromFactory } from "../hardhat/utils";
+
+/*
+    *******************************************************************************************
+    1. Set the VERIFIER_CONTRACT_NAME - e.g PlonkeVerifyFull
+    *******************************************************************************************
+    NB: use the verifier.address output as input for scripts/deployment/setVerifierAddress.ts 
+    *******************************************************************************************
+    npx hardhat run --network zkevm_dev scripts/deployment/deployVerifier.ts
+    *******************************************************************************************
+*/
+
+async function main() {
+  // const verifierFull = await deployFromFactory("PlonkVerifierFull");
+  // console.log(`PlonkVerifierFull deployed at ${verifierFull.address}`);
+
+  // await delay(30000);
+
+  // await run("verify", {
+  //   address: verifierFull.address,
+  // });
+
+
+  const verifierFullLarge = await deployFromFactory("PlonkVerifierFullLarge");
+  console.log(`PlonkVerifierFullLarge deployed at ${verifierFullLarge.address}`);
+
+  await delay(30000);
+
+  await run("verify", {
+    address: verifierFullLarge.address,
+  });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
