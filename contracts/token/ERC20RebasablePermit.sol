13712caea7a1cc2b0e17dc69a7a96fbcce30cfda
Artyom Veremeenko <arwer13@gmail.com>
2024-03-27 14:08:45 +0300

    feat(steth): intermediate work on adding ERC-2612/EIP-1271 permit

---
+++ contracts/token/ERC20RebasablePermit.sol
@@ -0,0 +1,159 @@
+// SPDX-FileCopyrightText: 2024 OpenZeppelin, Lido <info@lido.fi>
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.10;
+
+// import {UnstructuredStorage} from "@aragon/os/contracts/common/UnstructuredStorage.sol";
+
+// import {SignatureUtils} from "../common/lib/SignatureUtils.sol";
+// import {IEIP712ERC20Rebasable} from "../lib/IEIP712ERC20Rebasable.sol";
+
+import {UnstructuredStorage} from "./UnstructuredStorage.sol";
+import {ERC20Rebasable} from "./ERC20Rebasable.sol";
+import {EIP712} from "@openzeppelin/contracts-v4.9/utils/cryptography/EIP712.sol";
+import {SignatureChecker} from "../lib/SignatureChecker.sol";
+
+
+/**
+ * @dev Interface of the ERC20 Permit extension allowing approvals to be made via signatures, as defined in
+ * https://eips.ethereum.org/EIPS/eip-2612[EIP-2612].
+ *
+ * Adds the {permit} method, which can be used to change an account's ERC20 allowance (see {IERC20-allowance}) by
+ * presenting a message signed by the account. By not relying on {IERC20-approve}, the token holder account doesn't
+ * need to send a transaction, and thus is not required to hold Ether at all.
+ */
+interface IERC2612 {
+    /**
+     * @dev Sets `value` as the allowance of `spender` over ``owner``'s tokens,
+     * given ``owner``'s signed approval.
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `deadline` must be a timestamp in the future.
+     * - `v`, `r` and `s` must be a valid `secp256k1` signature from `owner`
+     * over the EIP712-formatted function arguments.
+     * - the signature must use ``owner``'s current nonce (see {nonces}).
+     */
+    function permit(
+        address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s
+    ) external;
+
+    /**
+     * @dev Returns the current nonce for `owner`. This value must be
+     * included whenever a signature is generated for {permit}.
+     *
+     * Every successful call to {permit} increases ``owner``'s nonce by one. This
+     * prevents a signature from being used multiple times.
+     */
+    function nonces(address owner) external view returns (uint256);
+
+    /**
+     * @dev Returns the domain separator used in the encoding of the signature for {permit}, as defined by {EIP712}.
+     */
+    // solhint-disable-next-line func-name-mixedcase
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+}
+
+
+contract ERC20RebasablePermit is IERC2612, ERC20Rebasable, EIP712 {
+    using UnstructuredStorage for bytes32;
+
+    /**
+     * @dev Nonces for ERC-2612 (Permit)
+     */
+    mapping(address => uint256) internal noncesByAddress;
+
+    /**
+     * @dev Typehash constant for ERC-2612 (Permit)
+     *
+     * keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)")
+     */
+    bytes32 internal constant PERMIT_TYPEHASH =
+        0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+
+    // TODO: outline structured storage used because at least EIP712 uses it
+    // TODO: use custom errors
+
+    /// @param name_ The name of the token
+    /// @param symbol_ The symbol of the token
+    /// @param decimals_ The decimals places of the token
+    /// @param wrappedToken_ address of the ERC20 token to wrap
+    /// @param tokenRateOracle_ address of oracle that returns tokens rate
+    /// @param bridge_ The bridge address which allowd to mint/burn tokens
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_,
+        address wrappedToken_,
+        address tokenRateOracle_,
+        address bridge_
+    )
+        ERC20Rebasable(name_, symbol_, decimals_, wrappedToken_, tokenRateOracle_, bridge_)
+        EIP712("Liquid staked Ether 2.0", "1")
+    {
+    }
+
+    /**
+     * @dev Sets `value` as the allowance of `spender` over ``owner``'s tokens,
+     * given ``owner``'s signed approval.
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `deadline` must be a timestamp in the future.
+     * - `v`, `r` and `s` must be a valid `secp256k1` signature from `owner`
+     * over the EIP712-formatted function arguments.
+     * - the signature must use ``owner``'s current nonce (see {nonces}).
+     */
+    function permit(
+        address _owner, address _spender, uint256 _value, uint256 _deadline, uint8 _v, bytes32 _r, bytes32 _s
+    ) external {
+        if (block.timestamp > _deadline) {
+            revert ErrorDeadlineExpired();
+        }
+
+        bytes32 structHash = keccak256(
+            abi.encode(PERMIT_TYPEHASH, _owner, _spender, _value, _useNonce(_owner), _deadline)
+        );
+
+        bytes32 hash = _hashTypedDataV4(structHash);
+
+        if (!SignatureChecker.isValidSignatureNow(_owner, hash, _v, _r, _s)) {
+            revert ErrorInvalidSignature();
+        }
+        _approve(_owner, _spender, _value);
+    }
+
+    /**
+     * @dev Returns the current nonce for `owner`. This value must be
+     * included whenever a signature is generated for {permit}.
+     *
+     * Every successful call to {permit} increases ``owner``'s nonce by one. This
+     * prevents a signature from being used multiple times.
+     */
+    function nonces(address owner) external view returns (uint256) {
+        return noncesByAddress[owner];
+    }
+
+    /**
+     * @dev Returns the domain separator used in the encoding of the signature for {permit}, as defined by {EIP712}.
+     */
+    // solhint-disable-next-line func-name-mixedcase
+    function DOMAIN_SEPARATOR() external view returns (bytes32) {
+        return _domainSeparatorV4();
+    }
+
+    /**
+     * @dev "Consume a nonce": return the current value and increment.
+     */
+    function _useNonce(address _owner) internal returns (uint256 current) {
+        current = noncesByAddress[_owner];
+        noncesByAddress[_owner] = current + 1;
+    }
+
+    error ErrorInvalidSignature();
+    error ErrorDeadlineExpired();
+}
---
+++ test/token/ERC20Permit.unit.test.ts
@@ -0,0 +1,214 @@
+import hre from "hardhat";
+import { assert } from "chai";
+import { unit } from "../../utils/testing";
+import { wei } from "../../utils/wei";
+import { makeDomainSeparator, signPermit } from "../../utils/testing/permit-helpers";
+
+import {
+    ERC20Bridged__factory,
+    TokenRateOracle__factory,
+    OssifiableProxy__factory,
+    ERC20RebasablePermit__factory,
+    ERC1271PermitSignerMock__factory,
+} from "../../typechain";
+import { BigNumber } from "ethers";
+
+
+const TOKEN_NAME = 'Liquid staked Ether 2.0'
+const TOKEN_VERSION = '1'
+
+// derived from mnemonic: want believe mosquito cat design route voice cause gold benefit gospel bulk often attitude rural
+const ACCOUNTS_AND_KEYS = [
+  {
+    address: '0xF4C028683CAd61ff284d265bC0F77EDd67B4e65A',
+    privateKey: '0x5f7edf5892efb4a5cd75dedd496598f48e579b562a70eb1360474cc83a982987',
+  },
+  {
+    address: '0x7F94c1F9e4BfFccc8Cd79195554E0d83a0a5c5f2',
+    privateKey: '0x3fe2f6bd9dbc7d507a6cb95ec36a36787706617e34385292b66c74cd39874605',
+  },
+]
+
+const getAccountsEOA = async () => {
+  return {
+    alice: ACCOUNTS_AND_KEYS[0],
+    bob: ACCOUNTS_AND_KEYS[1],
+  }
+}
+
+const getAccountsEIP1271 = async () => {
+  const deployer = (await hre.ethers.getSigners())[0]
+  const alice = await new ERC1271PermitSignerMock__factory(deployer).deploy()
+  const bob = await new ERC1271PermitSignerMock__factory(deployer).deploy()
+  return { alice, bob }
+}
+
+// const signPermit = async (owner, spender, value, nonce, domainSeparator, deadline, acct) => {
+//   const digest = calculatePermitDigest(owner, spender, value, nonce, domainSeparator, deadline)
+//   return await sign(digest, acct)
+// }
+
+
+unit("ERC20Permit", ctxFactory)
+
+  .test("wrappedToken() :: has the same address is in constructor", async (ctx) => {
+      const { rebasableProxied, wrappedToken } = ctx.contracts;
+      assert.equal(await rebasableProxied.WRAPPED_TOKEN(), wrappedToken.address)
+  })
+
+  .test('eip712Domain() is correct', async (ctx) => {
+    const token = ctx.contracts.rebasableProxied
+    const [ fields, name, version, chainId, verifyingContract, salt, extensions ] = await token.eip712Domain()
+
+    assert.equal(name, TOKEN_NAME)
+    assert.equal(version, TOKEN_VERSION)
+    assert.isDefined(hre.network.config.chainId)
+    assert.equal(chainId.toNumber(), hre.network.config.chainId as number)
+    assert.equal(verifyingContract, token.address)
+
+    const domainSeparator = makeDomainSeparator(TOKEN_NAME, TOKEN_VERSION, chainId, token.address)
+    assert.equal(makeDomainSeparator(name, version, chainId, verifyingContract), domainSeparator)
+  })
+
+  .test('DOMAIN_SEPARATOR() is correct', async (ctx) => {
+    const token = ctx.contracts.rebasableProxied
+    const [ fields, name, version, chainId, verifyingContract, salt, extensions ] = await token.eip712Domain()
+
+    assert.equal(name, TOKEN_NAME)
+    assert.equal(version, TOKEN_VERSION)
+    assert.isDefined(hre.network.config.chainId)
+    assert.equal(chainId.toNumber(), hre.network.config.chainId as number)
+    assert.equal(verifyingContract, token.address)
+
+    const domainSeparator = makeDomainSeparator(TOKEN_NAME, TOKEN_VERSION, chainId, token.address)
+    assert.equal(await ctx.contracts.rebasableProxied.DOMAIN_SEPARATOR(), domainSeparator)
+  })
+
+  .test('grants allowance when a valid permit is given', async (ctx) => {
+    const token = ctx.contracts.rebasableProxied
+
+    const { owner, spender, deadline } = ctx.permitParams
+    let { value } = ctx.permitParams
+    // create a signed permit to grant Bob permission to spend Alice's funds
+    // on behalf, and sign with Alice's key
+    let nonce = 0
+    const charlie = ctx.accounts.user2
+    const charlieSigner = hre.ethers.provider.getSigner(charlie.address)
+    // const bobSigner = hre.ethers.provider.getSigner(BOB.address)
+
+    const domainSeparator = makeDomainSeparator(TOKEN_NAME, TOKEN_VERSION, hre.network.config.chainId as number, token.address)
+    let { v, r, s } = await signPermit(owner, spender.address, value, nonce, deadline, domainSeparator)
+
+    // check that the allowance is initially zero
+    assert.equalBN(await token.allowance(owner.address, spender.address), BigNumber.from(0))
+    // check that the next nonce expected is zero
+    assert.equalBN(await token.nonces(owner.address), BigNumber.from(0))
+    // check domain separator
+    assert.equal(await token.DOMAIN_SEPARATOR(), domainSeparator)
+
+    // a third-party, Charlie (not Alice) submits the permit
+    // TODO: handle unpredictable gas limit somehow better than setting it a random constant
+    const tx = await token.connect(charlieSigner)
+      .permit(owner.address, spender.address, value, deadline, v, r, s, { gasLimit: '0xffffff' })
+
+    // check that allowance is updated
+    assert.equalBN(await token.allowance(owner.address, spender.address), BigNumber.from(value))
+    await assert.emits(token, tx, 'Approval', [ owner, spender, value ])
+    assert.equalBN(await token.nonces(owner.address), BigNumber.from(1))
+
+    // increment nonce
+    nonce = 1
+    value = 4e5
+    ;({ v, r, s } = await signPermit(owner, spender.address, value, nonce, deadline, domainSeparator))
+
+    // submit the permit
+    const tx2 = await token.connect(charlieSigner).permit(owner.address, spender.address, value, deadline, v, r, s)
+
+    // check that allowance is updated
+    assert.equalBN(await token.allowance(owner.address, spender.address), BigNumber.from(value))
+    assert.emits(token, tx2, 'Approval', [ owner.address, spender, BigNumber.from(value) ] )
+    assert.equalBN(await token.nonces(owner.address), BigNumber.from(2))
+  })
+
+  .run();
+
+async function ctxFactory() {
+    // const name = "StETH Test Token";
+    const name = TOKEN_NAME;
+    const symbol = "StETH";
+    const decimalsToSet = 18;
+    const decimals = BigNumber.from(10).pow(decimalsToSet);
+    const rate = BigNumber.from('12').pow(decimalsToSet - 1);
+    const premintShares = wei.toBigNumber(wei`100 ether`);
+    const premintTokens = BigNumber.from(rate).mul(premintShares).div(decimals);
+
+    const [
+        deployer,
+        owner,
+        recipient,
+        spender,
+        holder,
+        stranger,
+        user1,
+        user2,
+    ] = await hre.ethers.getSigners();
+
+    const wrappedToken = await new ERC20Bridged__factory(deployer).deploy(
+        "WsETH Test Token",
+        "WsETH",
+        decimalsToSet,
+        owner.address
+    );
+    const tokenRateOracle = await new TokenRateOracle__factory(deployer).deploy(
+        owner.address,
+        86400
+    );
+    const rebasableTokenImpl = await new ERC20RebasablePermit__factory(deployer).deploy(
+      name,
+      symbol,
+      decimalsToSet,
+      wrappedToken.address,
+      tokenRateOracle.address,
+      owner.address
+    );
+
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [hre.ethers.constants.AddressZero],
+    });
+
+    const zero = await hre.ethers.getSigner(hre.ethers.constants.AddressZero);
+
+    const l2TokensProxy = await new OssifiableProxy__factory(deployer).deploy(
+      rebasableTokenImpl.address,
+      deployer.address,
+      ERC20RebasablePermit__factory.createInterface().encodeFunctionData("initialize", [
+        name,
+        symbol,
+      ])
+    );
+
+    const rebasableProxied = ERC20RebasablePermit__factory.connect(
+      l2TokensProxy.address,
+      holder
+    );
+
+    await tokenRateOracle.connect(owner).updateRate(rate, 1000);
+    await rebasableProxied.connect(owner).mintShares(holder.address, premintShares);
+    // const { alice, bob } = await getAccountsEOA();
+    const { alice, bob } = await getAccountsEIP1271();
+
+    const MAX_UINT256 = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
+    return {
+      accounts: { deployer, owner, recipient, spender, holder, stranger, zero, user1, user2 },
+      constants: { name, symbol, decimalsToSet, decimals, premintShares, premintTokens, rate },
+      contracts: { rebasableProxied, wrappedToken, tokenRateOracle },
+      permitParams: {
+        owner: alice,
+        spender: bob,
+        value: 6e6,
+        nonce: 0,
+        deadline: MAX_UINT256,
+      }
+    };
+}
